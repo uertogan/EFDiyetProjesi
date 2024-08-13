d5aaabe2046f1915931af907edfc6afcdb99a82f
Ümmühan Ertoğan <ummuhan.ertogan@gmail.com>
2024-08-13 13:47:11 +0300

    Add project files.

---
+++ DiyetProje.DAL/Migrations/20240813101542_second.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiyetProje.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(2024, 8, 13, 13, 15, 41, 874, DateTimeKind.Local).AddTicks(89), 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Description",
+                value: "Süt, yoğurt, peynir, CreatedDate = DateTime.Now, Status = Enums.DataStatus.Added");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(2024, 8, 13, 13, 15, 41, 874, DateTimeKind.Local).AddTicks(92), 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(2024, 8, 13, 13, 15, 41, 874, DateTimeKind.Local).AddTicks(94), 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Meals",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(2024, 8, 13, 13, 15, 41, 874, DateTimeKind.Local).AddTicks(42), 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Meals",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(2024, 8, 13, 13, 15, 41, 874, DateTimeKind.Local).AddTicks(46), 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Meals",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(2024, 8, 13, 13, 15, 41, 874, DateTimeKind.Local).AddTicks(48), 1 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "CreatedDate", "DeletedDate", "Description", "ModifiedDate", "Picture", "PortionUnit", "ProductName", "Status" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "1 litre su 1 porsiyona denk", null, null, 1, "Su", 0 },
+                    { 2, 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "4 tane kayısı 1 porsiyona denk", null, null, 1, "Kayısı", 0 },
+                    { 3, 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "2 dilim elma 1 porsiyona denk", null, null, 1, "Elma", 0 },
+                    { 4, 4, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "350 gram pişmaniye 1 porsiyona denk", null, null, 1, "Pişmaniye", 0 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Gender", "Status" },
+                values: new object[] { new DateTime(2024, 8, 13, 13, 15, 41, 873, DateTimeKind.Local).AddTicks(9724), "Kadın", 1 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Description",
+                value: "Süt, yoğurt, peynir");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Meals",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Meals",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Meals",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "Status" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Gender", "Status" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "EK", 0 });
+        }
+    }
+}
